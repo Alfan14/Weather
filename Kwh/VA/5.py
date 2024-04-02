8592ab34e33a19b2ff78cb465294ab031343388e
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2024-04-03 06:53:24 +0700

    First commit**

---
+++ Kwh/VA/5.py
@@ -0,0 +1,14 @@
+#Perhitungan option
+division= 3500
+num3 =0
+print("=======Masukkan Watt masing-masing alat listrik dirumah========")
+while True:
+    sum = input("Masukkan angka (ketik 'stop' untuk berhenti): ")
+    if sum.lower() == 'stop':
+        break
+    print("====Masukkan Penggunaan alat listrik dirumah per jam====")
+    sum2 = input("Masukkan angka kedua(ketik 'stop' untuk berhenti): ")
+    lenght=float(sum)+float(sum2)
+    num3 +=lenght/division
+    print("Total penggunaan:", num3,"Kwh")
+    
